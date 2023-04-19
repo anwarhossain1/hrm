7362be65b6c68296be683a4ddb4bfbc266a3f4dd
Anwar Hossain <irishkhan33@gmail.com>
2023-04-19 15:49:57 +0600

    form added and one api route added

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+app.get('/hello', (req,res)=>{
+    res.send("hello world")
+})
+app.listen(1337, ()=>{
+    console.log('server started at port number 1337')
+})
