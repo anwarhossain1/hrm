03bd96c96ab5cbd64f2a95c9850cbea62d5f5d72
Anwar Hossain <irishkhan33@gmail.com>
2023-04-28 12:35:00 +0600

    add new employee form added

--- server/routes/admin/login.js
+++ server/routes/admin/login.js
@@ -25,32 +25,29 @@ router.post('/register', async (req, res)=>{
 
     }
 })
-
-// router.post('/login', async (req, res) => {
-//     try {
-//         const user = await User.findOne({
-//             name: req.body.name,
-//             pass: req.body.pass
-//         })
-//         if (user) {
-//             const token = jwt.sign({
-//                 name:user.name,
-//                 email:user.email
-//             },'dbslab1234')
-//             res.json({
-//                 status: 200,
-//                 message: 'Login Successful',
-//                 token:token
-//             })
-//         } else {
-//             res.json({
-//                 status: 203,
-//                 message: 'User doesnt exist'
-//             })
-//         }
-//     } catch (err) {
-//         console.log(err)
-//     }
-// })
+router.post('/login', async (req, res) => {
+    try {
+        const user = await adminSchema.findOne({
+            email: req.body.email,
+            password: req.body.pass
+        })
+        if (user) {
+            const token = jwt.sign({
+                email:user.email
+            },'admin123')
+            res.json({
+                status: 200,
+                message: 'Login Successful',
+                token:token
+            })
+        } else {
+            res.status(203).json({
+                message: 'User doesnt exist'
+            })
+        }
+    } catch (err) {
+        console.log(err)
+    }
+})
 
 module.exports = router
